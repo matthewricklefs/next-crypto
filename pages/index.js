586b6db72eb7f5e7a7e1a4f77029c341d5b9e69a
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-04-22 18:30:36 -0400

    imports useState into the index.js, creates allCoins variable and filters through the filteredCoins. creates handleChange and adds it to the Search component so that we can filtered through the coins in our CoinList

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import CoinList from "../components/CoinList";
 import Layout from "../components/Layout";
 import Search from "../components/Search";
 
 export default function Home({ filteredCoins }) {
+  const [search, setSearch] = useState("");
+
+  const allCoins = filteredCoins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setSearch(e.target.value.toLowerCase());
+  };
   return (
     <Layout>
       <div className="coin_app">
-        <Search type="text" placeholder="Search" />
-        <CoinList filteredCoins={filteredCoins} />
+        <Search type="text" placeholder="Search" onChange={handleChange} />
+        <CoinList filteredCoins={allCoins} />
       </div>
     </Layout>
   );
